a35c04e7bd75f5b2dd3e571d5f4ec186d96200a1
kaustubh Bajpai <144876616+Kaustubhbajpai7777@users.noreply.github.com>
2024-10-13 22:59:53 +0530

    Add files via upload

---
+++ D-14-Easy-NOTEBOOK.py
@@ -0,0 +1,13 @@
+# cook your dish here
+# Read the number of test cases
+T = int(input())
+
+for _ in range(T):
+    # Read the weight of the pulp for each test case
+    N = int(input())
+    
+    # Calculate the number of notebooks
+    notebooks = N * 10
+    
+    # Print the result for each test case
+    print(notebooks)
